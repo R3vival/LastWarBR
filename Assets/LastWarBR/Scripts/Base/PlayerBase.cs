dc8c6eb04a667d3419bc3c1f1150aba19c4ec347
R3-Santiago <s.gomez@getaclub.io>
2022-03-03 22:11:37 -0500

    Player base script Edit

--- Assets/LastWarBR/Scripts/Base/PlayerBase.cs
+++ Assets/LastWarBR/Scripts/Base/PlayerBase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,33 @@ namespace LastWarBR
 {
     public class PlayerBase : MonoBehaviour
     {
-        
+        #region Declarations
+        [SerializeField] protected string playerName;
+        [SerializeField] protected short health = 250;
+
+        //Actions
+        [SerializeField] public Action<short> LoseHealth;
+        [SerializeField] public Action<short> GainHealth;
+        #endregion
+        #region Functions
+        /// <summary>
+        /// Heals the player with the given points
+        /// the basic points healed ar 50 pts
+        /// </summary>
+        /// <param name="pointsHealed"></param>
+        protected void HealPlayer(short pointsHealed = 50)
+        {
+            health += pointsHealed;
+        }
+        /// <summary>
+        /// Deals damage to the player with the given points
+        /// the basic damage points are 50
+        /// </summary>
+        /// <param name="damage"></param>
+        public void TakeDamage(short damage = 50)
+        {
+            health -= damage;
+        }
+        #endregion
     }
 }
