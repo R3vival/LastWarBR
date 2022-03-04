e92da34e65ee9dc59b12262f917c7cff43da9c73
R3-Santiago <s.gomez@getaclub.io>
2022-03-03 19:44:23 -0500

    Player Initial implementation

---
+++ Assets/LastWarBR/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LastWarBR
+{
+    public class PlayerController : PlayerBase
+    {
+
+        [SerializeField] private GameObject PlayerView;
+
+        private void Start()
+        {
+        }
+    }
+}
